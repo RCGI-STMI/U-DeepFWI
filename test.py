b56cdc8298c54baf83926eff1b0935693616c42a
faizao <jonas.mendonca@dce.ufpb.br>
2020-07-30 14:02:29 -0300

    update script to run in distributed systems

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 from func.paramConfig import param #object with parameters
 from func.pathConfig import path #object with path
-from func.libConfig import tf,loadTest,generate_predicoes
+from func.libConfig import tf,loadTest,generate_predicoes,time
+
+start  = time.time()
 
 X_test,Y_test = loadTest() #loading data
 
-model = tf.keras.models.load_model('models/modelCurrent.h5')
-
+tf.debugging.set_log_device_placement(True)
+strategy = tf.distribute.MirroredStrategy()
+with strategy.scope():
+    model = tf.keras.models.load_model('models/modelCurrent.pb')
 preds_test = model.predict(X_test, batch_size=param.testBatchSize)
 
-generate_predicoes(preds_test,Y_test)
+final_time = time.time() - start
+generate_predicoes(preds_test,Y_test,final_time)
