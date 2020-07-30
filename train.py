666bcb759ef2d4d1580d8df9725dd19d4b92387c
faizao <jonas.mendonca@dce.ufpb.br>
2020-07-30 14:02:45 -0300

    update the train to run in distributed systems

--- train.py
+++ train.py
@@ -1,20 +1,22 @@
 from func.paramConfig import param #contains all parameters
-from func.pathConfig import path #contains all paths
 from func.libConfig import tf,time,loadTrain,create_model,save_time
 
-start  = time.time() 
+start = time.time()
 
 X_train,Y_train= loadTrain() #loading data
 
-if param.useTransferLearning == True:
-    print ('Loading model pre-trained')
-    model = tf.keras.models.load_model('models/model.h5') #Applying Tranfer learning
-
-else:
-    model = create_model() 
+tf.debugging.set_log_device_placement(True)
+strategy = tf.distribute.MirroredStrategy()
+with strategy.scope():
+    if param.useTransferLearning == True:
+        print('LOADING MODEL PRE-TRAINED')
+        model = tf.keras.models.load_model('models/model.pb') #Applying Tranfer learning
+    else:
+        model = create_model()
     
 model.fit(X_train, Y_train, batch_size=param.batchSize, epochs=param.epochs)
-model.save('models/modelCurrent.h5')
+print('SAVING MODEL')
+model.save('models/modelCurrent.pb')
 
-
-save_time(time.time() - start);
+final_time = time.time() - start
+save_time(final_time)
