e4ade45eecb08b2e670f11aa5f07423eef3a4f9e
faizao <jonas.mendonca@dce.ufpb.br>
2020-07-30 13:49:54 -0300

    include more informations in save data

--- func/libsJ.py
+++ func/libsJ.py
@@ -1,20 +1,29 @@
 import os
 import scipy.io as sio
+from datetime import datetime
 from func.paramConfig import param #object with all parameters
 
 
-def generate_predicoes(predictions,GT): # 
-    time_aux = sio.loadmat('./results/time.mat')
-    time_aux = time_aux['time']
+def generate_predicoes(predictions,GT,time_test): #
+    time_training = sio.loadmat('./results/time.mat') #import time of the training
+    time_training = time_training['time']
     data = {}
     data['GT'] = GT
     data['prediction'] = predictions
     data['param'] = param
-    data['time_training'] = time_aux
+    data['date'] = datetime.now().isoformat()
+    data['time_training'] = time_training #save the time of the training
+    data['time_testing'] = get_time_test(time_test) #save the time of the testing too
+
     
     print('SAVING RESULTS')
     sio.savemat('./results/results.mat',data)
 
+
+def get_time_test(timec):
+    return 'Testing complete in {:.0f}m  {:.0f}s'.format(timec // 60, timec % 60)
+
 def save_time(timec):
     aux_time='Training complete in {:.0f}m  {:.0f}s' .format(timec //60 , timec % 60)
     sio.savemat('./results/time.mat',{'time':aux_time})
+
